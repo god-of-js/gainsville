2a45c3d4e192c405f5d687a1137dca16c79101b9
Henry <hernysleek51@gmail.com>
2019-11-21 03:12:19 -0800

    11/21/2019-2

--- src/components/homenav.vue
+++ src/components/homenav.vue
@@ -4,31 +4,32 @@
       <v-toolbar-title>GAINSVILLE CHILDREN ACADEMY</v-toolbar-title>
       <!-- <v-spacer></v-spacer> -->
       <div class="flex-grow-1"></div>
-      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="icon"></v-app-bar-nav-icon> -->
-      <!-- <v-col class="d-flex justify-space-between options"> -->
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="icon"></v-app-bar-nav-icon>
+      <!-- d-flex justify-space-between  -->
+      <v-col class="options d-flex justify-end">
         <!-- <v-btn to="/home" text>Home</v-btn> -->
-        <!-- <v-btn to="/about" text>About</v-btn>
-        <v-btn to="/staff" text>Staffs</v-btn>
+        <v-btn to="/about" text>About</v-btn>
+        <!-- <v-btn to="/staff" text>Staffs</v-btn>
         <v-btn to="/location" text>Location</v-btn>
         <v-btn to="/activities" text>Activities</v-btn>
         <v-btn to="/publicinfo" text>Info</v-btn> -->
         <v-btn to="/studentdashboard" active rounded color="green" class="ml-2">Portal</v-btn>
-      <!-- </v-col> -->
+      </v-col>
     </v-app-bar>
 
     <!-- sidebar based on responsivity -->
-    <!-- <v-navigation-drawer v-model="drawer" absolute right temporary app >
+     <v-navigation-drawer v-model="drawer" absolute right temporary app >
       <v-list nav dense>
         <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
           <v-list-item to="/home" >
             <v-list-item-title>Home</v-list-item-title>
-          </v-list-item> -->
+          </v-list-item> 
 
-          <!-- <v-list-item to="/about">
+           <v-list-item to="/about">
             <v-list-item-title>About</v-list-item-title>
           </v-list-item>
 
-          <v-list-item to="/staff">
+        <!--  <v-list-item to="/staff">
             <v-list-item-title>Staffs</v-list-item-title>
           </v-list-item>
 
@@ -43,13 +44,13 @@
           <v-list-item to="/publicinfo">
             <v-list-item-title>Information</v-list-item-title>
           </v-list-item> -->
-<!--           
+          
           <v-list-item to="/studentdashboard">
             <v-list-item-title>Student Portal</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
-    </v-navigation-drawer> -->
+    </v-navigation-drawer> 
   </div>
 </template>
 
@@ -58,42 +59,42 @@
   display: none;
 }
 /* Extra small devices (phones, 600px and down) */
-/* @media only screen and (max-width: 600px) {
+@media only screen and (max-width: 600px) {
   .options {
     display: none;
   }
-} */
+}
 
 /* Small devices (portrait tablets and large phones, 600px and up) */
-/* @media only screen and (min-width: 600px) {
+@media only screen and (min-width: 600px) {
   .options {
     display: none;
   }
-} */
+}
 
 /* Medium devices (landscape tablets, 768px and up) */
-/* @media only screen and (min-width: 768px) {
+@media only screen and (min-width: 768px) {
   .options {
     display: none;
   }
-} */
+}
 
 /* Large devices (laptops/desktops, 992px and up) */
-/* @media only screen and (max-width: 992px) {
+@media only screen and (max-width: 992px) {
   .options {
     display: none !important;
   }
   .icon {
     display: block;
   }
-} */
+}
 
-/* Extra large devices (large laptops and desktops, 1200px and up) */
-/* @media only screen and (min-width: 1200px) {
+/* Extra large devices (large laptops and desktops, 1200px and up)  */
+@media only screen and (min-width: 1200px) {
   .options {
-    display: none;
+    display: block;
   }
-} */
+}
 </style>
 
 <script>
